6b029a440d72f3adf8ceb212e3d2836c19f910ac
CompBioLevings <dlevings@gmail.com>
2026-02-11 15:27:10 -0600

    Updated [shell]: updated shell script that runs workflow - archive-workflow.sh

--- archive-workflow.sh
+++ archive-workflow.sh
@@ -8,9 +8,11 @@ exclude_regex='(trim)|(forward)|(reverse)'
 data_fastq='data-fastq.txt'
 external_data_fastq='external-data-fastq.txt'
 combined_fastq='all-fastq-archive-list.txt'
-ignore_files='pipeline/data-archiver/ignored-files.txt'
-archive_mapping='pipeline/data-archiver/archived-mapping.txt'
+ignore_files='pipeline/data-archiver/data/ignored-files.txt'
+archive_mapping='pipeline/data-archiver/data/archived-mapping.txt'
 filtered_fastq='filtered-fastq.txt'
+archive_additions='new-fastq-archive-mapping.txt'
+num_workers=4
 
 # Activate conda environment
 conda activate base
@@ -28,6 +30,34 @@ rm "$data_fastq" "$external_data_fastq"
 # been archived (or should be ignored), using the archive mapping file.
 python ~/pipeline/data-archiver/filter_new_files.py -i "$combined_fastq" -g "$ignore_files" -a "$archive_mapping" -o "$filtered_fastq"
 
+# Exit script here if there are no new files to archive.
+if [ ! -s "$filtered_fastq" ]; then
+    echo "No new files to archive. Exiting."
+    rm "$combined_fastq" "$filtered_fastq"
+    exit 0
+fi
+
 # Get the list of files to archive and use archive_organizer.py to create a tab-delimited file mapping 
 # files to archive locations.
-python ~/pipeline/data-archiver/archive_organizer.py "$filtered_fastq" -o new-fastq-archive-mapping.txt
+python ~/pipeline/data-archiver/archive_organizer.py "$filtered_fastq" -o "$archive_additions" --ignore-file "$ignore_files"
+rm "$combined_fastq" "$filtered_fastq"
+
+# Now sync to the Tier 2 storage (using s3cmd) based on the generated mapping file.
+python ~/pipeline/data-archiver/s3_sync.py "$archive_additions" --workers "$num_workers"
+
+# Finally add the new archive additions to the 'master' archive mapping file.  Make sure to remove any lines that 
+# start with "original_file_path" (i.e. the header line) from the additions file before appending to the master mapping file.
+grep -v '^original_file_path' "$archive_additions" >> "$archive_mapping"
+rm "$archive_additions"
+
+# Now get the date and time of the last successful archive sync, then create a GZIP-compressed file of the 
+# archive mapping file and ignore file with a timestamp in the archive name for record-keeping. Use 7z to #
+# encrypt and add password to the archive mapping file and ignored file - take the password as user input
+timestamp=$(date +"%Y%m%d_%H%M%S")
+read -s -p "Enter password to encrypt archive mapping and ignored files: " password && echo ""
+7za a -p"$password" "pipeline/data-archiver/data/archiver-db-bkup_${timestamp}.7z" "$archive_mapping" "$ignore_files"
+unset password
+
+# To 'reset' in case of error, change dir to pipeline and extract database zipped archive and overwrite old files
+# cd ~/pipeline/data-archiver/data/
+# 7za x "archiver-db-bkup_*.7z"
